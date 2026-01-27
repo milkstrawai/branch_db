7b68081a5302be98d49eddad0424e7801b363dec
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2026-01-28 00:12:41 +0300

    Change cloning test DB to only create it

--- lib/branch_db/railtie.rb
+++ lib/branch_db/railtie.rb
@@ -14,7 +14,11 @@ module BranchDb
       next unless Rails.env.test?
 
       db_configs = ActiveRecord::Base.configurations.configs_for(env_name: Rails.env)
-      db_configs.each { |db_config| Preparer.new(db_config).prepare_if_needed }
+      db_configs.each do |db_config|
+        ActiveRecord::Tasks::DatabaseTasks.create(db_config)
+      rescue ActiveRecord::DatabaseAlreadyExists
+        # already exists, nothing to do
+      end
     end
   end
 end
